827e13a13af6d101209aa557b7fbf066cd30674b
tak <109452865+taku10101@users.noreply.github.com>
2023-06-07 01:59:32 +0900

    Update todo.tsx

--- src/todo.tsx
+++ src/todo.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react';
 
 
-function Todo() {
+function Todo(){
     const[inputText,setInputText]=useState("");
     //<Todo[]>　ここの配列にはTodoで指定した型、プロパティしか入らないようになる
     const [todos,setTodos]=useState<Todo[]>([]);
@@ -15,14 +15,18 @@ function Todo() {
     };
 //型　クイックフィックスで自動補完
 const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    e.preventDefault();
     //eで取ってきた値をsetInputValueに渡す
     setInputText(e.target.value);
 };
 
-const handleSubmit=(e: FormEvent<HTMLFormElement>)=>{
+const handleSubmit=(e:{ preventDefault: () => void })=>{
     //リロード阻止
     e.preventDefault();
 
+    if (!inputText) {
+        return;
+    }
     //新しいTodoを作成
     const newTodo: Todo ={//事前に設定した型の参照
     inputValue:inputText,
@@ -32,25 +36,34 @@ const handleSubmit=(e: FormEvent<HTMLFormElement>)=>{
     //作成した物をtodosに追加
     setTodos([newTodo, ...todos]);
     //投稿後空  に戻す
-    setInputText("");
+    setInputText(inputText);
     };
 const handleEdit=(id:Number,inputValue:string)=>{
-    const newTodos=todos.map((task)=>{
-        //inputでとってきたidがidと同じだった場合
-        if(task.id===id){
-            task.inputValue=inputValue;
+    const newTodos=todos.map((todo)=>{
+        if(todo.id===id){
+            todo.inputValue=inputValue;
         }
-        return task;
+        return todo;
     });
     setTodos(newTodos);
 };
+
 const handleChecked=(id:Number, checked:boolean)=>{
-    const newTodos=todos.map((task)=>{
-        if(task.id=== id){
-            task.checked= !checked;
+    const newTodos=todos.map((todo)=>{
+        if(todo.id=== id){
+            todo.checked= !checked;
         }
-        return task;
+        return todo;
 })};
+
+//消すやつ
+const handleDelete =(id:number)=>{
+    const newTodos = todos.filter((todo)=> todo.id!==id);
+    setTodos(newTodos);
+};
+
+
+
     return (
     <div className="to">
         <div>
@@ -61,24 +74,26 @@ const handleChecked=(id:Number, checked:boolean)=>{
                     {/* ボタンが押されたら */}
                 <input type="submit" value="追加" className='submitButton' />
             </form>
+            
             <ul>
-                {/* todosの中身をmapで1つずつ取り出してtaskに渡す */}
-                {todos.map((task)=>(
-                    // newTodo のinputValue
-                    <li key={task.id}>
-                        <input type="text" onChange={(e)=>{handleEdit(task.id,e.target.value)}} 
+                {/* todosの中身をmapで1つずつ取り出してtodoに渡す */}
+                {todos.map((todo)=>(
+                    <li key={todo.id}>
+                        <input type="text" onChange={(e)=>{handleEdit(todo.id,e.target.value)}} 
                         className='inputText'
-                        value={task.inputValue}
-                        disabled ={task.checked}
-                        />
-                    <input type="checkbox" onChange={()=>{handleChecked(task.id,task.checked)}} 
-                        className='inputText'
-                        value={task.inputValue}/>
+                        value={todo.inputValue}
+                        disabled ={todo.checked}/>
+
+                    <input type="checkbox" onChange={()=>{handleChecked(todo.id,todo.checked)}} className='inputText' />
+                    <button onClick={()=> handleDelete(todo.id)}>消</button>
                     </li>
                 ))}
             </ul>
             </div>
         </div>
-    );
+        );
 };
     export default Todo;
+
+
+
